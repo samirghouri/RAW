54d49ad5ece5498449bc4257abda5b6f32e6e4df
samirghouri <samira.ghouri@st.niituniversity.in>
2020-09-14 14:29:12 +0530

    Updates

---
+++ precache-manifest.57c4e94d89430b4ece4ae9d5705bd676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd54b200cd9b3d51848de7666ba639b",
+    "url": "/RAW/index.html"
+  },
+  {
+    "revision": "d79ada608b6642c6c51c",
+    "url": "/RAW/static/css/main.f6f2a768.chunk.css"
+  },
+  {
+    "revision": "2ce0742a14d679cd1d87",
+    "url": "/RAW/static/js/2.847f9d43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RAW/static/js/2.847f9d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79ada608b6642c6c51c",
+    "url": "/RAW/static/js/main.38d3439b.chunk.js"
+  },
+  {
+    "revision": "932b87a15a1d7007a23f",
+    "url": "/RAW/static/js/runtime-main.a3ff6bca.js"
+  }
+]);
